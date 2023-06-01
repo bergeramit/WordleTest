096065921d6588bf259b2fbca2696e2ff6cf1afd
Amit Berger <bergeramit@gmail.com>
2023-06-01 18:08:37 +0300

    added express

---
+++ wordle_clone/app.js
@@ -0,0 +1,26 @@
+const express = require("express"); 
+const path = require('path');
+const app = express(); // Initializing Express App
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/script.js', function(req, res){
+    res.sendFile(__dirname + '/public/script.js');
+});
+
+app.get('/words.js', function(req, res){
+    res.sendFile(__dirname + '/public/words.js');
+});
+
+app.get('/nakama.js', function(req, res){
+    res.sendFile(__dirname + '/public/nakama.js');
+});
+
+app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/public/style.css');
+});
+
+app.listen(3000, ()=> console.log("App Listening on port 3000"));
